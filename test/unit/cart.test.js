6b86b097604ea6f596931704b9113ee644bd5186
Denis <d.medvedev@author24.ru>
2021-10-22 19:00:46 +0300

    Add cart test

--- test/unit/cart.test.js
+++ test/unit/cart.test.js
@@ -1,34 +1,22 @@
 import React from "react";
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { it, expect, describe } from '@jest/globals';
-import events from '@testing-library/user-event';
 import { Cart } from '../../src/client/pages/Cart'
 import { initStore, addToCart } from '../../src/client/store';
 import { MockExampleApi, CartApi } from './mocks';
 
 describe('корзина', () => {
-    it("в корзине есть товар", async () => {
+    it("продукт добавляется в корзину", async () => {
         const basename = "/hw/store";
         const api = new MockExampleApi(basename);
         const cart = new CartApi();
         const product = await api.getProductById(0);
 
-        // TODO: что происходит :D
-        let count = 0;
-
-        cart.setState({
-            [product.data.id]: {
-                name: product.data.name,
-                count,
-                price: product.data.price
-            }
-        });
-
-        cart.storage[product.data.id].count ++;
-
         const store = initStore(api, cart);
+
+        store.dispatch(addToCart(product.data))
 
         const application = (
             <BrowserRouter basename={basename}>
@@ -40,6 +28,46 @@ describe('корзина', () => {
 
         addToCart(product.data);
 
-        const { container } = render(application);
+        render(application);
+
+        expect(store.getState().cart).toStrictEqual({
+            [product.data.id]: {
+                name: product.data.name,
+                price: product.data.price,
+                count: 1,
+            }
+        })
+    });
+
+    it("продукт добавляется в корзину повторно", async () => {
+        const basename = "/hw/store";
+        const api = new MockExampleApi(basename);
+        const cart = new CartApi();
+        const product = await api.getProductById(0);
+
+        const store = initStore(api, cart);
+
+        store.dispatch(addToCart(product.data));
+        store.dispatch(addToCart(product.data));
+
+        const application = (
+            <BrowserRouter basename={basename}>
+                <Provider store={store}>
+                    <Cart />
+                </Provider>
+            </BrowserRouter>
+        );
+
+        addToCart(product.data);
+
+        render(application);
+
+        expect(store.getState().cart).toStrictEqual({
+            [product.data.id]: {
+                name: product.data.name,
+                price: product.data.price,
+                count: 2,
+            }
+        })
     });
 })
