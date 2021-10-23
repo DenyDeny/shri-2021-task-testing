c69f7026c5e0a418d31c1d3d0cdc87e366cbe4e8
Denis <d.medvedev@author24.ru>
2021-10-23 23:23:08 +0300

    reload cart auto test

--- test/hermione/desktop.hermione.js
+++ test/hermione/desktop.hermione.js
@@ -22,3 +22,27 @@ describe('статичная отрисовка страниц', async function(
         });
     });
 });
+
+describe('корзина', async function() {
+    it('содержимое сохраняется после перезагрузки', async function() {
+        // Add item to a cart for start
+        await this.browser.url('/hw/store/catalog/1');
+        await this.browser.$('.Product button').click();
+
+        // Go to the cart
+        await this.browser.url('/hw/store/cart');
+        // const stateBeforeReload = await this.browser.$('.Cart tbody tr');
+        // Reload page
+        await this.browser.url('https://google.ru/');
+        await this.browser.url('/hw/store/cart');
+
+        this.browser.waitUntil(async () => {
+            await this.browser.assertView('plain', '.Cart', {
+                compositeImage: true,
+            });
+        }, 5000, 'waiting 5s');
+        // const stateAfterReload = await this.browser.$('.Cart tbody tr');
+        //
+        // assert.equal(stateBeforeReload, stateAfterReload, 'состояние корзины не изменилось');
+    });
+});
