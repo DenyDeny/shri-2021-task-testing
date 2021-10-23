5bacae98ea0d373ac78e7fce5d457ad4b95dc451
Denis <d.medvedev@author24.ru>
2021-10-23 22:02:27 +0300

    unit test for catalog

--- test/unit/catalog.test.tsx
+++ test/unit/catalog.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { it, expect, describe } from '@jest/globals';
 import events from '@testing-library/user-event';
 import { Application } from '../../src/client/Application';
@@ -57,5 +57,37 @@ describe('каталог', () => {
 
         const products = container.querySelectorAll('.card-body');
         expect(products.length).toEqual(2);
+    });
+
+    it("у товара отображается название, цена и ссылка", async () => {
+        const basename = "/hw/store";
+        const api = new MockExampleApi(basename);
+        const cart = new CartApi();
+        const store = initStore(api, cart);
+
+        const application = (
+            <BrowserRouter basename={basename}>
+                <Provider store={store}>
+                    <Application />
+                </Provider>
+            </BrowserRouter>
+        );
+
+        const { container } = render(application);
+        const catalog = container.querySelector('.nav-link:nth-child(1)');
+
+        events.click(catalog);
+
+        await (function (ms) {
+            return new Promise((res) => setTimeout(() => res(1), ms));
+        })(100);
+
+        const products = container.querySelectorAll('.card-body');
+        expect(Array.from(products).map(product => {
+            const name = product.querySelector('.card-title').textContent;
+            const price = product.querySelector('.card-text').textContent.replace(/[^+\d]/g, '');
+            const link = product.querySelector('.card-link').textContent;
+            return { name, price: Number(price), link };
+        })).toStrictEqual([{ name: 'a', price: 100, link: 'Details' }, { name: 'b', price: 200, link: 'Details' }]);
     });
 })
