3397ab72a7e85d61b74397521e18e6e57d6a7fdf
Denis <d.medvedev@author24.ru>
2021-10-24 02:58:49 +0300

    uncomment

--- test/hermione/desktop.hermione.js
+++ test/hermione/desktop.hermione.js
@@ -1,73 +1,73 @@
-// const { assert } = require('chai');
-//
-// describe('статичная отрисовка главной страницы', async function() {
-//     it('desktop', async function() {
-//         await this.browser.url('/hw/store/');
-//         await this.browser.assertView('plain', '.Application', {
-//             compositeImage: true,
-//         });
-//     });
-//
-//     it('mobile', async function() {
-//         await this.browser.setWindowSize(480, 920);
-//         await this.browser.url('/hw/store/');
-//         await this.browser.assertView('plain', '.Application', {
-//             compositeImage: true,
-//         });
-//     });
-// });
-//
-// describe('статичная отрисовка страницы доставки', async function() {
-//     it('desktop', async function() {
-//         await this.browser.url('/hw/store/delivery');
-//         await this.browser.assertView('plain', '.Application', {
-//             compositeImage: true,
-//         });
-//     });
-//
-//     it('mobile', async function() {
-//         await this.browser.setWindowSize(480, 920);
-//         await this.browser.url('/hw/store/delivery');
-//         await this.browser.assertView('plain', '.Application', {
-//             compositeImage: true,
-//         });
-//     });
-// });
-//
-// describe('статичная отрисовка страницы контакты', async function() {
-//     it('desktop', async function() {
-//         await this.browser.url('/hw/store/contacts');
-//         await this.browser.assertView('plain', '.Application', {
-//             compositeImage: true,
-//         });
-//     });
-//
-//     it('mobile', async function() {
-//         await this.browser.setWindowSize(480, 920);
-//         await this.browser.url('/hw/store/contacts');
-//         await this.browser.assertView('plain', '.Application', {
-//             compositeImage: true,
-//         });
-//     });
-// });
-//
-// describe('корзина', async function() {
-//     it('содержимое сохраняется после перезагрузки', async function() {
-//         // Add item to a cart for start
-//         await this.browser.url('/hw/store/catalog/1');
-//         await this.browser.$('.Product button').click();
-//
-//         // Go to the cart
-//         await this.browser.url('/hw/store/cart');
-//
-//         // Reload page
-//         await this.browser.url('https://google.ru/');
-//         await this.browser.url('/hw/store/cart');
-//
-//         this.browser.waitUntil(async () => {
-//             await this.browser.assertView('plain', '.Cart', {
-//                 compositeImage: true,
-//             });
-//         }, 5000, 'waiting 5s');
-//     });
-// });
+const { assert } = require('chai');
+
+describe('статичная отрисовка главной страницы', async function() {
+    it('desktop', async function() {
+        await this.browser.url('/hw/store/');
+        await this.browser.assertView('plain', '.Application', {
+            compositeImage: true,
+        });
+    });
+
+    it('mobile', async function() {
+        await this.browser.setWindowSize(480, 920);
+        await this.browser.url('/hw/store/');
+        await this.browser.assertView('plain', '.Application', {
+            compositeImage: true,
+        });
+    });
+});
+
+describe('статичная отрисовка страницы доставки', async function() {
+    it('desktop', async function() {
+        await this.browser.url('/hw/store/delivery');
+        await this.browser.assertView('plain', '.Application', {
+            compositeImage: true,
+        });
+    });
+
+    it('mobile', async function() {
+        await this.browser.setWindowSize(480, 920);
+        await this.browser.url('/hw/store/delivery');
+        await this.browser.assertView('plain', '.Application', {
+            compositeImage: true,
+        });
+    });
+});
+
+describe('статичная отрисовка страницы контакты', async function() {
+    it('desktop', async function() {
+        await this.browser.url('/hw/store/contacts');
+        await this.browser.assertView('plain', '.Application', {
+            compositeImage: true,
+        });
+    });
+
+    it('mobile', async function() {
+        await this.browser.setWindowSize(480, 920);
+        await this.browser.url('/hw/store/contacts');
+        await this.browser.assertView('plain', '.Application', {
+            compositeImage: true,
+        });
+    });
+});
+
+describe('корзина', async function() {
+    it('содержимое сохраняется после перезагрузки', async function() {
+        // Add item to a cart for start
+        await this.browser.url('/hw/store/catalog/1');
+        await this.browser.$('.Product button').click();
+
+        // Go to the cart
+        await this.browser.url('/hw/store/cart');
+
+        // Reload page
+        await this.browser.url('https://google.ru/');
+        await this.browser.url('/hw/store/cart');
+
+        this.browser.waitUntil(async () => {
+            await this.browser.assertView('plain', '.Cart', {
+                compositeImage: true,
+            });
+        }, 5000, 'waiting 5s');
+    });
+});
--- test/hermione/touch.hermione.js
+++ test/hermione/touch.hermione.js
@@ -1,19 +1,19 @@
 const { assert } = require('chai');
 
 describe('гамбургер', async function() {
+    it('скрывается при разрешении больше или равно 576px', async function() {
+        await this.browser.url('/hw/store/');
+        await this.browser.setWindowSize(800, 800)
+        const burger = await this.browser.$('.navbar-toggler');
+
+        assert.equal(await burger.isDisplayed(), false);
+    });
+
     it('появляется при разрешении меньше 576px', async function() {
         await this.browser.url('/hw/store/');
         await this.browser.setWindowSize(575, 800)
         const burger = await this.browser.$('.navbar-toggler');
 
         assert.equal(await burger.isDisplayed(), true);
-    });
-
-    it('скрывается при разрешении больше или равно 576px', async function() {
-        await this.browser.url('/hw/store/');
-        await this.browser.setWindowSize(576, 800)
-        const burger = await this.browser.$('.navbar-toggler');
-
-        assert.equal(await burger.isDisplayed(), false);
     });
 });
